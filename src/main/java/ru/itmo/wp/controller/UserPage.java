901a13ae02adfbde63d6fb6d0529b0e64206de9e
lexzhe <lexzhe@mail.ru>
2020-03-01 12:32:51 +0300

    Initial commit

---
+++ src/main/java/ru/itmo/wp/controller/UserPage.java
@@ -0,0 +1,25 @@
+package ru.itmo.wp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import ru.itmo.wp.domain.User;
+import ru.itmo.wp.service.UserService;
+
+@Controller
+public class UserPage extends Page {
+    private final UserService userService;
+
+    public UserPage(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/user/{id:\\d{1,18}}")
+    public String user(Model model, @PathVariable("id") User user) {
+
+        model.addAttribute("currentUser", user);
+        return "UserPage";
+
+    }
+}
